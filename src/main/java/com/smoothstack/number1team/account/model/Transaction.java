2a23a611f31a3586f38b0142f1799a56508cd196
blaze vincent <hikikomareep@pop-os.localdomain>
2023-02-15 13:21:55 -0600

    Add JPA annotations

--- src/main/java/com/smoothstack/number1team/account/model/Transaction.java
+++ src/main/java/com/smoothstack/number1team/account/model/Transaction.java
@@ -1,13 +1,25 @@
 package com.smoothstack.number1team.account.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "transaction")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     double id;
-    double sourceAccount;
-    double destinationAccount;
+
+    @Column(name = "sourceAccount")
+    Account sourceAccount;
+
+    @Column(nullable = false, name = "destinationAccount")
+    Account destinationAccount;
+
+    @Column(nullable = false)
     Long date;
 }
