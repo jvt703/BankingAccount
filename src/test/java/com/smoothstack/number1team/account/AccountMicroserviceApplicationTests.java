898fa03313ea43ee91d019cbfdc12943694ce139
blaze vincent <hikikomareep@pop-os.localdomain>
2023-02-14 21:52:44 -0600

    Add Spring Initializr files

---
+++ src/test/java/com/smoothstack/number1team/account/AccountMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.number1team.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
