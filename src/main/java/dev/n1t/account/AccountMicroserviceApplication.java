0e8be848a2d37031533e1fc772dacc9080005ba2
blaze vincent <hikikomareep@pop-os.localdomain>
2023-02-23 05:33:33 -0600

    Add entity scanning of model directory

--- src/main/java/dev/n1t/account/AccountMicroserviceApplication.java
+++ src/main/java/dev/n1t/account/AccountMicroserviceApplication.java
@@ -2,8 +2,10 @@ package dev.n1t.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("dev.n1t.model")
 public class AccountMicroserviceApplication {
 
 	public static void main(String[] args) {
