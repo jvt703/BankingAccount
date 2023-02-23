708eb150be7b21fa296a2ef736f7f07692ebe5be
blaze vincent <hikikomareep@pop-os.localdomain>
2023-02-23 05:36:19 -0600

    N1T-146, N1T-148, N1T 149 create, read, delete functionality

--- src/main/java/dev/n1t/account/controller/AccountController.java
+++ src/main/java/dev/n1t/account/controller/AccountController.java
@@ -1,19 +1,58 @@
 package dev.n1t.account.controller;
+import dev.n1t.account.dto.AccountRegistrationDto;
 import dev.n1t.account.repository.AccountRepository;
+import dev.n1t.account.service.AccountService;
 import dev.n1t.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController()
 public class AccountController {
 
-    @Autowired
-    AccountRepository accountRepository;
+    private final AccountService accountService;
 
-    @GetMapping(path = "/user/{userId}/accounts", produces = "application/json")
-    public List<Account> getUserAccounts(@PathVariable(value = "userId") User user){
-        return accountRepository.findByUser(userId)
+    @Autowired
+    public AccountController(AccountService accountService){
+        this.accountService = accountService;
     }
+
+    //get all accounts visible to a user
+    //todo: limit visibility to authorized user
+    @GetMapping(path = "/user/{userId}/accounts", produces = "application/json")
+    public List<Account> getUserAccounts(
+            @PathVariable(value = "userId") long userId
+    ){
+        return accountService.getAccountsByUserId(userId);
+    }
+
+    //get single account
+//    @GetMapping(path = "/user/{userId}/account/{accountId}", produces = "application/json")
+//    public List<Account> getUserAccounts(
+//            @PathVariable(value = "userId") long userId,
+//            @PathVariable(value = "accountId") long accountId
+//    ){
+//        return accountService.getAccountsByUserId(userId);
+//    }
+
+    //create new account (e.g. checking or savings)
+    @PostMapping("/user/{userId}/account")
+    public Account createAccount(
+            @PathVariable(value = "userId") long userId,
+            @Validated @RequestBody AccountRegistrationDto accountRegistrationDto
+    ){
+        return accountService.createAccount(accountRegistrationDto, userId);
+    }
+
+    //delete specific account
+    @DeleteMapping("/user/{userId}/account/{accountId}")
+    public Account deleteAccount(
+            @PathVariable(value = "userId") long userId,
+            @PathVariable(value = "accountId") long accountId
+    ) {
+        return accountService.deleteAccount(userId, accountId);
+    }
+
 }
