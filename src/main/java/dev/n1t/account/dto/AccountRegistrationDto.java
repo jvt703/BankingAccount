31aced12050c065176eed33ac4e336256c8a063d
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-03-02 01:45:48 -0600

    Add missing serialization lombok generators

--- src/main/java/dev/n1t/account/dto/AccountRegistrationDto.java
+++ src/main/java/dev/n1t/account/dto/AccountRegistrationDto.java
@@ -1,8 +1,10 @@
 package dev.n1t.account.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class AccountRegistrationDto {
     private Long accountTypeId;
     private String accountName;
