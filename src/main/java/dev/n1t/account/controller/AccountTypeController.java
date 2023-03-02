106205bda82b8f0249a1c9d7b1b29aa818da1f98
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-03-02 01:45:01 -0600

    Fix request body not being read into DTO

--- src/main/java/dev/n1t/account/controller/AccountTypeController.java
+++ src/main/java/dev/n1t/account/controller/AccountTypeController.java
@@ -5,8 +5,10 @@ import dev.n1t.account.repository.AccountTypeRepository;
 import dev.n1t.account.service.AccountTypeService;
 import dev.n1t.model.AccountType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -27,7 +29,7 @@ public class AccountTypeController {
     }
 
     @PostMapping(path = "/accountType", produces = "application/json")
-    public AccountType createAccountType(IncomingAccountTypeDto incomingAccountTypeDto){
+    public AccountType createAccountType(@Validated @RequestBody IncomingAccountTypeDto incomingAccountTypeDto){
         return this.accountTypeService.createAccountType(incomingAccountTypeDto);
     }
 }
