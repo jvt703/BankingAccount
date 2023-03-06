2d315806b626cf8a95b7dae5e117920aee653815
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-03-06 02:28:48 -0600

    Add missing @Repository annotation

--- src/main/java/dev/n1t/account/repository/CreditDetailRepository.java
+++ src/main/java/dev/n1t/account/repository/CreditDetailRepository.java
@@ -4,10 +4,12 @@ import dev.n1t.model.Account;
 import dev.n1t.model.CreditCardType;
 import dev.n1t.model.CreditDetail;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CreditDetailRepository extends CrudRepository<CreditDetail, Long> {
     Optional<CreditDetail> findByCreditCardTypeAndAccountIn(CreditCardType creditCardType, List<Account> accounts);
 }
