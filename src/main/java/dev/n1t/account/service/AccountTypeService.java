9495c29c35e71aab33ce994afad5430eca87e30a
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-03-06 01:53:12 -0600

    Remove dynamic accountType creation; Add accountType presets

--- src/main/java/dev/n1t/account/service/AccountTypeService.java
+++ src/main/java/dev/n1t/account/service/AccountTypeService.java
@@ -4,6 +4,8 @@ import dev.n1t.account.dto.IncomingAccountTypeDto;
 import dev.n1t.account.repository.AccountTypeRepository;
 import dev.n1t.model.AccountType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,16 +19,31 @@ public class AccountTypeService {
         this.accountTypeRepository = accountTypeRepository;
     }
 
+    @EventListener(ContextRefreshedEvent.class)
+    public void initializeAccountTypes() {
+        accountTypeRepository.deleteAll();
+
+        AccountType checking = new AccountType();
+        checking.setAccountTypeName("Checking");
+        checking.setDescription("General purpose account for everyday expenses");
+
+        AccountType savings = new AccountType();
+        savings.setAccountTypeName("Savings");
+        savings.setDescription("Account intended for saving funds");
+
+        AccountType credit = new AccountType();
+        credit.setAccountTypeName("Credit");
+        credit.setDescription("Account tied to a credit card with a rewards points system");
+
+        AccountType loan = new AccountType();
+        loan.setAccountTypeName("Loan");
+        loan.setDescription("Account whose balance reflects the owed amount on a loan");
+
+        accountTypeRepository.saveAll(List.of(checking, savings, credit, loan));
+    }
+
     public List<AccountType> getAllAccountTypes(){
         return this.accountTypeRepository.findAll();
     }
 
-    public AccountType createAccountType(IncomingAccountTypeDto incomingAccountTypeDto){
-        AccountType output = new AccountType();
-
-        output.setAccountTypeName(incomingAccountTypeDto.getAccountTypeName());
-        output.setDescription(incomingAccountTypeDto.getDescription());
-
-        return this.accountTypeRepository.save(output);
-    }
 }
