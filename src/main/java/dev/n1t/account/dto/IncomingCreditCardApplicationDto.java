2f8ca1f54f5a48399a0d87567e1a1ffcb12b6c42
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-03-12 19:48:45 -0500

    Annotate relationships; Standardize SSN type

---
+++ src/main/java/dev/n1t/account/dto/IncomingCreditCardApplicationDto.java
@@ -0,0 +1,25 @@
+package dev.n1t.account.dto;
+
+import dev.n1t.model.EmploymentStatus;
+import dev.n1t.model.ResidenceOwnershipStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IncomingCreditCardDto {
+    private long creditCardTypeId;
+
+    private long socialSecurityNumber;
+
+    private String motherMaidenName;
+
+    private ResidenceOwnershipStatus residenceOwnershipStatus;
+
+    private double housingPayment;
+
+    private EmploymentStatus employmentStatus;
+
+    private double grossAnnualIncome;
+
+}
