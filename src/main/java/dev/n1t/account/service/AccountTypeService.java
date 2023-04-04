fd248f35ba32eac930a9b45e4ffba39d203a1e4d
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-04-04 08:02:06 -0500

    Remove unused ArrayList

--- src/main/java/dev/n1t/account/service/AccountTypeService.java
+++ src/main/java/dev/n1t/account/service/AccountTypeService.java
@@ -1,6 +1,5 @@
 package dev.n1t.account.service;
 
-import dev.n1t.account.dto.IncomingAccountTypeDto;
 import dev.n1t.account.repository.AccountTypeRepository;
 import dev.n1t.model.AccountType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,6 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -26,34 +23,29 @@ public class AccountTypeService {
 
         List<AccountType> existingAccountTypes = accountTypeRepository.findAll();
         if(existingAccountTypes.isEmpty()){
-            List<AccountType> stagedAccountTypes = new ArrayList<>();
             AccountType checking = AccountType.builder()
                     .id(1L)
                     .accountTypeName("Checking")
                     .description("General purpose account for everyday expenses")
                     .build();
-            stagedAccountTypes.add(checking);
 
             AccountType savings = AccountType.builder()
                     .id(2L)
                     .accountTypeName("Savings")
                     .description("Account intended for saving funds")
                     .build();
-            stagedAccountTypes.add(savings);
 
             AccountType credit = AccountType.builder()
                     .id(3L)
                     .accountTypeName("Credit")
                     .description("Account tied to a credit card with a rewards points system")
                     .build();
-            stagedAccountTypes.add(credit);
 
             AccountType loan = AccountType.builder()
                     .id(4L)
                     .accountTypeName("Loan")
                     .description("Account whose balance reflects the owed amount on a loan")
                     .build();
-            stagedAccountTypes.add(loan);
 
             accountTypeRepository.saveAll(List.of(checking, savings, credit, loan));
         }
