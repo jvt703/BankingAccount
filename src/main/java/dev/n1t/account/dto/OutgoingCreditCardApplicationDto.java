a66c4a7ec55c8cbd5959c867c2c9b5e657337455
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-04-12 10:29:55 -0500

    Enable nullability of approval

--- src/main/java/dev/n1t/account/dto/OutgoingCreditCardApplicationDto.java
+++ src/main/java/dev/n1t/account/dto/OutgoingCreditCardApplicationDto.java
@@ -24,7 +24,7 @@ public class OutgoingCreditCardApplicationDto {
     private double housingPayment;
     private EmploymentStatus employmentStatus;
     private double grossAnnualIncome;
-    private boolean approved;
+    private Boolean approved;
     private Instant decisionDate;
 
     public OutgoingCreditCardApplicationDto(CreditCardApplication creditCardApplication){
