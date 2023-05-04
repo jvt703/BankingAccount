77c0397ccb7d76ac6ff00fe7a872a33ef1a90fd8
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-05-04 13:56:32 -0500

    Add service for sending emails

---
+++ src/main/java/dev/n1t/account/service/EmailServiceImpl.java
@@ -0,0 +1,32 @@
+package dev.n1t.account.service;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+
+@Component
+public class EmailServiceImpl {
+    @Autowired private JavaMailSender emailSender;
+    public void sendSimpleMessage(String to, String subject, String text) {
+        try {
+            MimeMessage message = emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setFrom(
+                    new InternetAddress(
+                            "ninetenemailserver@gmail.com",
+                            "NineTen Bank")
+            );
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+            emailSender.send(message);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            System.out.printf("An email could not be sent to %s%n", subject);
+        }
+    }
+}
